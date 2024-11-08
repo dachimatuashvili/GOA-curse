4c8ae40e52db9624b4768af5c7f17318ec7cafc2
dachimatuashvili <tam.zardiashvili@gmail.com>
2024-11-08 19:28:47 +0400

    Add files via upload

--- lesson10.py
+++ lesson10.py
@@ -1,4 +1,28 @@
+names=["vaxo", "badri", "zviadi", "levani","dachi" ]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+
+
+names=["vaxtangi", "vasili," "dachi", "badri", "zviadi", "giorgi", "nika", "levani", "cotne", "luka", ]
+for i in range (10):
+     print(names[i])
+
+
+
+
+
 numbers=[1, 4, 7, 10, 66, 21, 128, 44, 398, 4444, 6785848]
 for num in numbers:
     if num > 10:
      print(num)
+
+
+
+
+     random_items=(20, "car", True, 4.2, False)
+    for i in random_items:
+       print(i)
