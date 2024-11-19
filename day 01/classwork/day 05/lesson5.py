1360c5c1f8f98a146e36df510af7e1bccfef84c8
dachimatuashvili <tam.zardiashvili@gmail.com>
2024-11-19 15:53:47 +0400

    commit

---
+++ day 01/classwork/day 05/lesson5.py
@@ -0,0 +1,23 @@
+user_name="dachi"
+user_age=13
+user_hobby="playing football"
+user_balance="unknown"
+
+print(user_name)
+print(user_age)
+print(user_hobby)
+print(user_balance)
+
+
+
+
+user_number = 44
+print(user_number - 20 * 3)
+
+
+num1=24
+num2=99
+num3=45
+num4=4
+
+print(172 / 4)
