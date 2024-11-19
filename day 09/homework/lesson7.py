ab2c7b65c029f2b171a9dea651a59aca6bc374b1
dachimatuashvili <tam.zardiashvili@gmail.com>
2024-11-19 16:45:39 +0400

    commit

---
+++ day 09/homework/lesson7.py
@@ -0,0 +1,34 @@
+user_age = 49
+my_age = 13
+print(user_age / my_age)
+#მომხმარებლის ასაკში ჩემი ასაკი, 3-ჯერ მოთავსდება
+
+
+
+
+
+
+user_name = "gocha"
+my_name = "dachi"
+print(user_name <= my_name)
+
+
+
+
+
+
+
+num1 = 29
+num2 = 3
+print(num1 / num2)
+#ნაშთი იქნება 6
+
+
+
+
+
+
+someone_age = 76
+print(someone_age / 2)
+
+
