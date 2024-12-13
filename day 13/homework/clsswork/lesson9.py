6e5848dc1369a12df1848e4cde71264835f43e9f
dachimatuashvili <tam.zardiashvili@gmail.com>
2024-12-13 19:43:22 +0400

    commit

---
+++ day 13/homework/clsswork/lesson9.py
@@ -0,0 +1,80 @@
+for i in range(0,11):
+    print(i)
+
+
+
+
+
+for i in range(50,150):
+    print(i)
+
+
+
+
+
+for i in range(200,2,500):
+    print(i)
+
+
+
+
+
+for i in range(0,1,50):
+    print(i)    
+
+
+
+ 
+
+name=("zviadi")
+for i in name:
+    print(i)
+
+
+
+
+
+
+
+
+while i in range(10,15):
+    print(i)
+
+
+
+
+
+while i in range(50, 0):
+    print(i)
+
+
+
+
+while i in range(10, 2, 70):
+    print(i)
+
+
+
+
+
+
+
+while i in range(25,3,55):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
