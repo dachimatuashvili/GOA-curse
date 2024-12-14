91ecaf55c09a8f950b02e167004f55c6d1bd2ee4
dachimatuashvili <tam.zardiashvili@gmail.com>
2024-12-14 21:07:26 +0400

    commit

---
+++ day 23/homework/lesson13.py
@@ -0,0 +1,55 @@
+name = input("Please enter your name: ")
+
+name = name.lower()
+
+print("Your name in lowercase is:", name)
+
+
+
+
+
+
+
+surname = "matuashvili"
+
+surname = surname.upper()
+
+print("Your surname in uppercase is:", surname)
+
+
+
+
+
+
+text = "hello world"
+
+text = text.capitalize()
+
+print("Transformed text:", text)
+
+
+
+
+
+text = "Hello, world!"
+
+index = text.find('i')
+
+print("The index of 'i' is:", index)
+
+
+
+
+
+
+words = ["hello", "world", "python", "dachi", "awesome"]
+
+for i in range(len(words)):
+    words[i] = words[i].upper()
+
+print("Transformed words:", words)
+
+
+
+
+
